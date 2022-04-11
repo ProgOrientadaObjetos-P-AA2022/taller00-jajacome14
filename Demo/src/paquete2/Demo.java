653275961fb2aacaa94ba9c898be79364beb1e28
javier jacome <jajacome14tpl.edu.ec>
2022-04-11 09:40:46 -0500

    primer commit

--- Demo/src/paquete2/Demo.java
+++ Demo/src/paquete2/Demo.java
@@ -4,20 +4,18 @@
  * and open the template in the editor.
  */
 package paquete2;
+import paquete1.Mensaje;
+import paquete3.Valor;
 
-
-/**
- *
- * @author reroes
- */
 public class Demo {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        System.out.printf();
+                
+        System.out.printf("%s con %s\n",
+         Mensaje.obtenerFrase(),Valor.obtenerHabitantes());
+         
     }
-    
 }
